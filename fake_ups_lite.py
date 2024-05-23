edf34694d4f30ee1590cca6ab29ed0568eb29530
PinkChocolate <125756541+PinkChocolate@users.noreply.github.com>
2024-05-23 13:19:42 +0300

    fake_ups_lite.py

--- fake_ups_lite.py
+++ fake_ups_lite.py
@@ -41,7 +41,7 @@ class UPS:
     def capacity(self):
         try:
             address = 0x32
-            read = self._bus.read_word_data(address, 0x09)
+            read = self._bus.read_word_data(address, 0x0c)
             swapped = struct.unpack("<H", struct.pack(">H", read))[0]
             return swapped / 256
         except:
@@ -50,8 +50,8 @@ class UPS:
     def charging(self):
         try:
             GPIO.setmode(GPIO.BCM)
-            GPIO.setup(0x0c, GPIO.IN)
-            return '+' if GPIO.input(0x0c) == GPIO.HIGH else '-'
+            GPIO.setup(0x09, GPIO.IN)
+            return '+' if GPIO.input(0x09) == GPIO.HIGH else '-'
         except:
             return '-'
 
